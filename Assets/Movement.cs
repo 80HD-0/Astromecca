aae9c176972af15b7eb027082917bbbaf99aaddb
Melanie Pacheco <afunnierguy@gmail.com>
2025-02-23 20:30:28 -0800

    Fix a bug in the movement system where movement was not relative to direction

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,31 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.AccessControl;
-using System.Threading;
 using UnityEngine;
-
 
 public class Movement : MonoBehaviour
 {
     public float movementSpeed = 5.0f;
-    public float sensetivity = 1;
+    public float sensitivity = 1.0f;
     public Rigidbody playerRB;
     public Transform player;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get input axes
         float verticalAxis = Input.GetAxis("Vertical");
         float horizontalAxis = Input.GetAxis("Horizontal");
-        Vector3 moveDirection = player.forward * verticalAxis + player.right * horizontalAxis;
-        playerRB.velocity = new Vector3(horizontalAxis*movementSpeed, playerRB.velocity.y, verticalAxis*movementSpeed);
+
+        // Get mouse input for rotation
         float mouseX = Input.GetAxis("Mouse X");
-        player.Rotate(Vector3.up * (mouseX * sensetivity));
+
+        // Rotate the player based on mouse input (yaw rotation)
+        float xRot = mouseX * sensitivity * Time.deltaTime;
+        player.Rotate(Vector3.up * xRot);
+
+        // Move the player relative to the player's forward direction
+        Vector3 forward = player.forward * verticalAxis;
+        Vector3 right = player.right * horizontalAxis;
+        Vector3 movement = (forward + right).normalized * movementSpeed;
+
+        // Apply movement to the player's rigidbody
+        playerRB.velocity = new Vector3(movement.x, playerRB.velocity.y, movement.z);
     }
 }
